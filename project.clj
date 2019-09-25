776a40b0d0cb864bfac8eb3ab3ab32f7010d9fe9
James N. V. Cash <james.nvc@gmail.com>
2019-09-25 13:08:58 -0400

    Add test using reitit for routing

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject tada "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [metosin/spec-tools "0.10.0"]
-                 [org.clojure/core.match "0.3.0"]])
+                 [org.clojure/core.match "0.3.0"]
+                 [org.clojure/test.check "0.10.0"]]
+
+  :profiles {:test {:dependencies [[metosin/reitit "0.3.9"]]}})
--- test/tada/test/events/ring.clj
+++ test/tada/test/events/ring.clj
@@ -52,3 +52,37 @@
                (:status (handler {:params {:name "user-thing"}}))))
         (is (= 400
                (:status (handler {:params {:value 5}}))))))))
+
+(deftest routing-with-reitit
+  (let [db (atom [])]
+    (tada/register-events!
+      [{:id :add-event!
+        :params {:name string?
+                 :value integer?}
+        :conditions (fn [{:keys [name value]}]
+                      [[(or (string/starts-with? name "user-")
+                            (string/starts-with? name "system-"))
+                        :incorrect
+                        "Even name must begin with 'user-' or 'system-'"]
+                       [(<= 0 value) :incorrect "Value must be positive"]])
+        :effect (fn [{:keys [name value] :as arg}]
+                  (swap! db conj arg))}])
+    (let [app (ring/ring-handler
+                (ring/router
+                  ["/api"
+                   ["/event"
+                    {:post {:handler (tada-ring/route :add-event!)}}]]))]
+      (testing "Can call the handler"
+        (is (= {:status 200}
+               (app {:request-method :post
+                     :uri "/api/event"
+                     :params {:name "user-thing"
+                              :value 5}})))
+        (is (= {:status 200}
+               (app {:request-method :post
+                     :uri "/api/event"
+                     :params {:name "system-thing"
+                              :value 0}})))
+        (is (= [{:name "user-thing" :value 5}
+                {:name "system-thing" :value 0}]
+               @db))))))
