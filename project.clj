8f58fb3f8b6d6c317d7bf416cf0b5e2ed465c908
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-04-13 15:54:41 -0400

    Use lein-v for release management

--- project.clj
+++ project.clj
@@ -1,7 +1,14 @@
-(defproject tada "0.0.1-SNAPSHOT"
+(defproject tada :lein-v
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [metosin/spec-tools "0.10.0"]
                  [org.clojure/core.match "0.3.0"]
                  [org.clojure/test.check "0.10.0"]]
+
+  :plugins [[com.roomkey/lein-v "7.0.0"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["v" "update"]
+                  ["vcs" "push"]
+                  ["deploy" "clojars"]]
 
   :profiles {:test {:dependencies [[metosin/reitit "0.3.9"]]}})
