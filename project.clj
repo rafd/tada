b51b647291241e4f4f57a061a1d62166d2b079a9
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-04-13 15:58:12 -0400

    Add lein :description, :license and :url

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject tada :lein-v
+  :description "A clojure(script) library that helps you compose web-applications out of declarative data-driven parts"
+  :url "https://github.com/rafd/tada"
+  :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [metosin/spec-tools "0.10.0"]
                  [org.clojure/core.match "0.3.0"]
