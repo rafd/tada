bc9a8dd6e5fe6ad3e026170315003116428e6dfe
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-07-30 15:48:03 -0400

    Extract ring-dispatch-event! out of tada.events.ring/route

--- src/tada/events/ring.clj
+++ src/tada/events/ring.clj
@@ -2,20 +2,23 @@
   (:require
     [tada.events.core :as tada]))
 
- (defn route [ev-id]
+(defn ring-dispatch-event! [event-id event-params]
+  (try
+    (if-let [return (tada/do! event-id event-params)]
+      {:status 200
+       :body return}
+      {:status 200})
+    (catch clojure.lang.ExceptionInfo e
+      {:body (.getMessage e)
+       :status (case (:anomaly (ex-data e))
+                 :incorrect 400
+                 :forbidden 403
+                 :unsupported 405
+                 :not-found 404
+                 ;; if no anomaly (usually do to event :effect or :return throwing)
+                 ;; rethrow the exception
+                 (throw e))})))
+
+(defn route [event-id]
   (fn [request]
-    (try
-      (if-let [return (tada/do! ev-id (request :params))]
-        {:status 200
-         :body return}
-        {:status 200})
-      (catch clojure.lang.ExceptionInfo e
-        {:body (.getMessage e)
-         :status (case (:anomaly (ex-data e))
-                   :incorrect 400
-                   :forbidden 403
-                   :unsupported 405
-                   :not-found 404
-                   ;; if no anomaly (usually do to event :effect or :return throwing)
-                   ;; rethrow the exception
-                   (throw e))}))))
+    (ring-dispatch-event! event-id (request :params))))
